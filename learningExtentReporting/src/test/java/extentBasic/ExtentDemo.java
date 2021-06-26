945cdea603380d7e6bdd6cc8e8d8e1e8683d9376
nilesh <tripathinilesh.1991@gmail.com>
2021-06-26 03:38:44 -0400

    my first commit

---
+++ learningExtentReporting/src/test/java/extentBasic/ExtentDemo.java
@@ -0,0 +1,42 @@
+package extentBasic;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+
+public class ExtentDemo {
+	
+	ExtentReports extent;
+	
+	@BeforeTest
+	public void config() {
+		String path = System.getProperty("user.dir")+"\\reports\\index.html";
+		ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+		reporter.config().setDocumentTitle("Test Result");
+		reporter.config().setReportName("Automation Test Result");
+		
+		extent = new ExtentReports();
+		
+		extent.attachReporter(reporter);
+		extent.setSystemInfo("Tester", "Nilesh Tripathi");
+	}
+	
+	@Test
+	public void initialDemo() {
+		
+		ExtentTest test = extent.createTest("initialDemo");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Nilesh\\Desktop\\Udemy Selenium\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com");
+		System.out.println(driver.getTitle());
+		
+		test.fail("Result do not match");
+		driver.quit();
+		extent.flush();
+	}
+}
